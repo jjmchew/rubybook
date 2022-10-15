97dafea5067b93caca4749327156b6ac33c2a693
jjmchew <jjmchew@gmail.com>
2022-10-14 22:39:26 -0600

    methods complete

---
+++ 03methods/ex2.rb
@@ -0,0 +1,6 @@
+x = 2  #2
+puts x = 2 #nil (puts returns nil)
+p name = "Joe" #"Joe" (p returns expression output)
+four = "four" #"four"
+print something = "nothing" #nil  (print returns nil)
+
---
+++ 03methods/ex4-5.rb
@@ -0,0 +1,10 @@
+def scream(words)
+  words = words + "!!!!"
+  #update: return
+  puts words
+end
+
+scream("Yippeee")
+
+#initially returns nothing - empty return
+#after update, puts words to screen but returns nil (puts)
